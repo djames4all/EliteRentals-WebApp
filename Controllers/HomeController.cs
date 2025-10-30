3612782a75406b09acd4b886a00583e9da05e118
Daniel James <danjames4all@gmail.com>
2025-10-30 11:09:31 +0200

    Updated UI & Fixed Property Image View
    1) Login
    2) Register
    3) Home Page - UI
    4) Home Page - Properties
    5) About Us
    6) Our Services
    7) Property Listing - Fixed UI for Image Display.
    
    *I have a blank Contact Us page, will updated it with my stashed changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using EliteRentals.Models;
+using EliteRentals.Models.ViewModels;
+using EliteRentals.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EliteRentals.Controllers
@@ -7,13 +9,56 @@ namespace EliteRentals.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IEliteApi _api;
+        private readonly IConfiguration _cfg;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IEliteApi api, IConfiguration cfg)
         {
             _logger = logger;
+            _api = api;
+            _cfg = cfg;
         }
 
-        public IActionResult Index()
+        // Loads all properties, picks 3 random in the view
+        public async Task<IActionResult> Index(CancellationToken ct)
+        {
+            try
+            {
+                var all = await _api.GetPropertiesAsync(ct);
+
+                var vm = new PropertyListViewModel
+                {
+                    Items = all,
+                    TotalCount = all.Count,
+                    Query = new PropertyListQuery(),
+                    ApiBaseUrl = _cfg["ApiSettings:BaseUrl"] ?? ""
+                };
+
+                return View(vm);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error loading properties for home page");
+                var vm = new PropertyListViewModel
+                {
+                    Items = new List<EliteRentals.Models.DTOs.PropertyReadDto>(),
+                    TotalCount = 0,
+                    Query = new PropertyListQuery(),
+                    ApiBaseUrl = _cfg["ApiSettings:BaseUrl"] ?? ""
+                };
+                return View(vm);
+            }
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult OurServices()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
         {
             return View();
         }
